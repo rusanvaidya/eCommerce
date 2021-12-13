884a4d4d87b2da99c837f7eb0541ee076363c988
Rusan Vaidya <60218372+rusanvaidya@users.noreply.github.com>
2021-12-13 18:15:40 +0545

    Delete Main directory

--- Main/home/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
